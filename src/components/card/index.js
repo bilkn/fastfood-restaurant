03415f6799841b2b7cd96b2bac3858149e914deb
bilkn <bilkan.konus@hotmail.com>
2021-05-22 17:11:28 +0300

    style: card

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,19 +1,32 @@
 import React from 'react';
+import './index.css';
 
 export default function Card({ children, ...rest }) {
-  return <div {...rest}>{children}</div>;
+  return (
+    <div className="px-5 py-12" {...rest}>
+      {children}
+    </div>
+  );
 }
 
 Card.Content = function CardContent({ children, ...rest }) {
-  return <div {...rest}>{children}</div>;
+  return (
+    <div className="card__content p-5" {...rest}>
+      {children}
+    </div>
+  );
 };
 
 Card.List = function CardList({ children, ...rest }) {
-  return <ul {...rest}>{children}</ul>;
+  return <ul className="flex space-y-6 flex-col"{...rest}>{children}</ul>;
 };
 
 Card.Item = function CardItem({ children, ...rest }) {
-  return <li {...rest}>{children}</li>;
+  return (
+    <li className="rounded-xl overflow-hidden" {...rest}>
+      {children}
+    </li>
+  );
 };
 
 Card.Link = function CardLink({ children, href, ...rest }) {
@@ -29,9 +42,9 @@ Card.Img = function CardImg({ src, alt, ...rest }) {
 };
 
 Card.Title = function CardTitle({ children, ...rest }) {
-  return <h3 {...rest}>{children}</h3>;
+  return <h3 className="text-lg" {...rest}>{children}</h3>;
 };
 
 Card.Text = function CardText({ children, ...rest }) {
-  return <p {...rest}>{children}</p>;
+  return <p className="card__text" {...rest}>{children}</p>;
 };
